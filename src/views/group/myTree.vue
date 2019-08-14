b22795a17b2f3cc0619ecce2d06c603721c16954
adolfvitor <tong.shen@17dianjia.com>
2019-08-14 14:31:55 +0800

    搜索

--- src/views/group/myTree.vue
+++ src/views/group/myTree.vue
@@ -1,19 +1,50 @@
 <template>
   <div id="mytree" class="mytree container">
+    <header>
+      <el-input
+          style="width: 200px"
+          class="cellPhone-input"
+          placeholder="手机号查询"
+          v-model.trim="cellPhone"
+          clearable>
+        </el-input>
+        <el-button class="search-btn" type="primary" icon="el-icon-search" size="medium" @click="search_handle">搜索</el-button>
+    </header>
     <section>
-        <div v-for="(item, index) in treeList" :key="index">
-          <div @click="treeLabelclick(item, index)" >{{item.name}}</div>
-          <div v-if="index == meIndex">
-            <el-tree
-              :props="defaultProps"
-              :data="meTree"
-              ref="tree"
-              node-key="cellPhone"
-              @node-click="handleNodeClick"
-              >
-            </el-tree>
+      <div class="search-tree" v-if="showSearchTree">
+        <el-tree
+          empty-text=""
+          :props="defaultProps"
+          :data="searchTree"
+          ref="tree"
+          default-expand-all
+          node-key="cellPhone"
+          @node-click="handleNodeClick"
+          >
+        </el-tree>
+      </div>
+      <div v-show="!showSearchTree">
+        <div v-if="showTree">
+          <div v-for="(item, index) in treeList" :key="index">
+            <div class="item-name" >
+              <span  @click="treeLabelclick(item, index)" class="arrow-icon" :class="{'apend':item.append}"></span>
+              <span>{{item.name}}</span>
+            </div>
+            <div v-if="item.append">
+              <el-tree
+                empty-text=""
+                :props="defaultProps"
+                :data="treeList[index].child"
+                ref="tree"
+                default-expand-all
+                node-key="cellPhone"
+                @node-click="handleNodeClick"
+                >
+              </el-tree>
+            </div>
           </div>
         </div>
+      </div>
     </section>
   </div>
 </template>
@@ -27,8 +58,11 @@ export default {
   name: 'mytree',
   data () {
     return {
+      cellPhone: '',
       currentId: null,
       treeList: [],
+      searchTree: [],
+      showSearchTree: false,
       showTree: false,
       meIndex: 0,
       meTree: [],
@@ -36,7 +70,7 @@ export default {
       innerId: null,
       query: {
         pageNom: 1,
-        size: 100
+        size: 20
       },
       defaultProps: {
         children: 'children',
@@ -47,13 +81,7 @@ export default {
   components: {
   },
   activated () {
-    console.log('activated group')
     this.getGroupFirstLevel(this.query)
-
-    // this.getTreeByCellphone({ cellPhone: '18570862377' }).then(res => {
-    //   console.log(res)
-    //   this.meTree = [res]
-    // })
   },
   deactivated () {
     console.log('deactivated group')
@@ -67,22 +95,21 @@ export default {
     ...mapGetters(['userInfo'])
   },
   methods: {
+    search_handle () {
+      const cellPhone = '18570866688'
+      this.getTreeByCellphone({ cellPhone }).then(res => {
+        this.searchTree = [res]
+        this.showSearchTree = true
+      })
+    },
     treeLabelclick (item, index) {
       this.getTreeByCellphone({ cellPhone: item.cellPhone }).then(res => {
-        this.meIndex = index
-        this.meTree = [res]
-        // this.$set(this.treeList[index], 'children', []) 18570862898
-
-        // this.treeList[index].children = [res]
-        // console.log(this.treeList[index].children)
-        // this.treeList[index]
+        this.treeList[index].append = !this.treeList[index].append
+        this.treeList[index].child = [res]
       })
     },
     handleNodeClick (data) {
       console.log(data)
-      // this.getGroupFirstLevel(node).then(res => {
-
-      // })
     },
     loadNode1(node, resolve) {
       // 一级节点处理
@@ -125,13 +152,14 @@ export default {
       const getGroupFirstLevel = await this.$http.getGroupFirstLevel(params).catch()
       if (getGroupFirstLevel && getGroupFirstLevel.records) {
         const _records = getGroupFirstLevel.records
-        this.treeList = _records
 
-        _records.forEach(et => {
-          if (et.children === undefined) {
-            et.children = []
-          }
-        })
+        for (let i = 0; i < _records.length; i++) {
+          _records[i].child = []
+          _records[i].append = false
+        }
+        this.treeList = _records
+        console.log(this.treeList)
+        this.showTree = true
         // resolve(_records)
         // this.MyTree = _records
       } else {
@@ -169,7 +197,37 @@ export default {
 </script>
 <style lang="stylus" scoped>
   .mytree{
+    header{
+      height 60px
+      display flex
+      align-items center
+      .search-btn{
+        margin-left 30px
+      }
+    }
+    .item-name{
+      color #606266
+      cursor pointer
+      // height 26px
+      line-height 26px
+      .arrow-icon{
+        display inline-block
+        transform: rotate(0);
+                transition: transform .3s ease-in-out,-webkit-transform .3s ease-in-out;
 
+      }
+      .arrow-icon.apend{
+            transform: rotate(90deg);
+      }
+      .arrow-icon:before{
+            content: "\E791";
+            font-family: element-icons!important
+            color: #C0C4CC
+            font-size: 12px;
+            padding-right: 4px;
+            // transform: rotate(90deg);
+      }
+    }
   }
 </style>
 
